670a51ec256f2edb44406642dc3f63f278b032a9
lu.he <lu.he@jd.com>
2019-09-11 14:04:29 -0700

    fix pyx

--- pyflow.pyx
+++ pyflow.pyx
@@ -16,11 +16,17 @@ cdef extern from "src/Coarse2FineFlowWrapper.h":
                                 int nSORIterations, int colType,
                                 int h, int w, int c);
 
-cdef extern from "src/SplatMotionsWrapper.h":
+cdef extern from "src/Coarse2FineFlowWrapper.h":
     void SplatMotionsWrapper(double *vx, double *vy,
                              const double *vxForward, const double *vyForward,
                              const double *vxBackward, const double *vyBackward,
                              const double * Im1, const double * Im2,
+                             int colType, int h, int w, int c, double t);
+
+cdef extern from "src/Coarse2FineFlowWrapper.h":
+    void ColorTransferWrapper(double *dest, const double *flow,
+                             const double *Im1, const double *Im2,
+                             const double *forward, const double *backward,
                              int colType, int h, int w, int c, double t);
 
 
@@ -97,4 +103,38 @@ def splat_motions(np.ndarray[double, ndim=2, mode="c"] vxForward not None,
     return vx, vy
 
 
+def color_transfer(np.ndarray[double, ndim=3, mode="c"] Im1 not None,
+                   np.ndarray[double, ndim=3, mode="c"] Im2 not None,
+                   np.ndarray[double, ndim=3, mode="c"] forward not None,
+                   np.ndarray[double, ndim=3, mode="c"] backward not None,
+                   np.ndarray[double, ndim=3, mode="c"] flow not None,
+                   double t = 0.5, int colType=0):
 
+
+
+    """
+    Output Format:
+      double * vx, double * vy,
+      const double * vxForward, const double * vyForward,
+      const double * vxBackward, const double * vyBackward,
+      const double * Im1 (range [0,1]), const double * Im2 (range [0,1]),
+      int colType (0 or default:RGB, 1:GRAY),
+      flost t,
+    Images Format: (h,w,c): float64: [0,1]
+    """
+    cdef int h = Im1.shape[0]
+    cdef int w = Im1.shape[1]
+    cdef int c = Im1.shape[2]
+    cdef np.ndarray[double, ndim=3, mode="c"] dest = \
+        np.ascontiguousarray(np.zeros((h, w, 3), dtype=np.float64))
+    flow = np.ascontiguousarray(flow)
+    Im1 = np.ascontiguousarray(Im1)
+    Im2 = np.ascontiguousarray(Im2)
+    forward = np.ascontiguousarray(forward)
+    backward = np.ascontiguousarray(backward)
+
+    ColorTransferWrapper(&dest[0, 0, 0], &flow[0, 0, 0],
+                         &Im1[0, 0, 0], &Im2[0, 0, 0],
+                         &forward[0, 0, 0], &backward[0, 0, 0],
+                         colType, h, w, c, t)
+    return dest
